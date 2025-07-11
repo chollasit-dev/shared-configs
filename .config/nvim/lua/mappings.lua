bf3974cf24e653519e6bd13d2ee3502432280db7
chollasit-dev <118034127+chollasit-dev@users.noreply.github.com>
2025-07-11 23:28:24 +0700

    Add shortcuts to quick notes and frequent used dirs

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -98,3 +98,8 @@ end, { desc = "previous todo comment" })
 map("n", "<leader>pat", "<cmd>GoAddTag<CR>", { desc = "add Go tag" })
 map("n", "<leader>pfs", "<cmd>GoFillStruct<CR>", { desc = "auto fill Go struct" })
 map("n", "<leader>pie", "<cmd>GoIfErr<CR>", { desc = "add Go if err" })
+
+-- custom
+map("n", "<leader><A-1>", "<cmd>cd ~/Documents<CR>", { desc = "goto notes directory" })
+map("n", "<leader><A-2>", "<cmd>cd ~/projects/personal/playground<CR>", { desc = "goto code playground directory" })
+map("n", "<leader><A-3>", "<cmd>cd ~/projects/jobs/level11/pman<CR>", { desc = "goto main job directory" })
