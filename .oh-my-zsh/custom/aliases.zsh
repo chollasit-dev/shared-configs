05da325bcb3d9ee621062ddca57bcd04e4629f88
chollasit-dev <118034127+chollasit-dev@users.noreply.github.com>
2025-08-02 23:34:12 +0700

    Add new and remove unnecessary aliases
    - Add necessary custom Tmux related alias to help streamline workflows
      when in rush
    - Remove aliases which only save a few keystrokes

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -1,14 +1,12 @@
-#!/usr/bin/env sh
-
 alias c="clear && history -p"
-alias g="git"
 alias glog="git log --graph --topo-order --pretty='%w(100,0,6)%C(yellow)%h%C(bold)%C(black)%d %C(cyan)%ar %C(green)%an%n%C(bold)%C(white)%s %N' --abbrev-commit"
 # TODO: Check `lsd` config file.
 alias ls2="lsd -AFl --tree --depth=2 --total-size --group-directories-first --blocks=user,size,name,links,git --hyperlink=always"
 alias ls="lsd -AFl --total-size --group-directories-first --blocks=user,size,name,links,git --hyperlink=always"
-alias lsIng="lsd -AFl --tree --depth=2 --total-size --group-directories-first --blocks=user,size,name,links,git -I .git -I node_modules --hyperlink=always"
-alias n="nvim"
+alias lsing="lsd -AFl --tree --depth=2 --total-size --group-directories-first --blocks=user,size,name,links,git -I .git -I node_modules --hyperlink=always"
+alias ne="cd $HOME/shared-configs/.config/nvim/ && nvim"
 alias t1='tree -aFpsu -L 1'
 alias t2='tree -aFpsu -L 2'
+alias t="tmux-sessionizer"
+alias te="cd $(dirname $(which tmux-sessionizer)) && nvim $(basename $(which tmux-sessionizer))"
 alias wget-staticweb='wget --convert-links --mirror --page-requisites --no-clobber --no-parent --domains'
-alias y='yazi'
