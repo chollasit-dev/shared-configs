3219981d49b488b3350410422631acf323f9c18c
chollasit-dev <118034127+chollasit-dev@users.noreply.github.com>
2025-08-02 18:41:23 +0700

    Add Tmux configs and plugins including which works with Neovim

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -311,4 +311,27 @@ return {
       },
     },
   },
+
+  ------------------------------
+  --- external interactivity ---
+  ------------------------------
+  -- tmux
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+      "TmuxNavigatorProcessList",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<CR>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<CR>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<CR>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<CR>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<CR>" },
+    },
+  },
 }
