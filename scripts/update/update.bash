3ec8db89e600f2a1e218709c5ae2949e1fb89190
chollasit-dev <118034127+chollasit-dev@users.noreply.github.com>
2026-01-01 02:55:38 +0700

    Add update Go and Node.js script

---
+++ scripts/update/update.bash
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+
+if command -v go &>/dev/null; then
+  read -p "Should clean Go cache? [y/n] " -r option
+  case $option in
+  "y" | "Y")
+    go clean -cache -modcache
+    ;;
+  esac
+fi
+
+UPDATE_NODE_SCRIPTS="$HOME/shared-configs/scripts/update/update_fnm.zsh"
+[ -f "$UPDATE_NODE_SCRIPTS" ] && "$UPDATE_NODE_SCRIPTS"
+
+command -v yarn &>/dev/null && yarn cache clean --all
+command -v pnpm &>/dev/null && pnpm cache delete
+corepack cache clean && corepack install -g pnpm@latest && corepack enable
+
+pnpm up -i && pnpm up -iL
+rm pnpm-lock.yaml yarn.lock package-lock.json pnpm-workspace.yaml &>/dev/null
+pnpm up -gL
+pnpm approve-builds
+pnpm approve-builds -g
