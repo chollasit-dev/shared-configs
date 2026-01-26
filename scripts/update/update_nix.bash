6e80b90961037a2d0b2951d65499578acfae9695
chollasit-dev <118034127+chollasit-dev@users.noreply.github.com>
2026-01-27 01:44:21 +0700

    Refactor script directory from `init` to `install`

--- scripts/update/update_nix.bash
+++ scripts/update/update_nix.bash
@@ -14,5 +14,5 @@ INSTALLED_VERSION="$(nix --version | sed 's/[^0-9.]//g')"
 sudo rm -rf "/nix" "$HOME/.nix-channels" "$HOME/.nix-defexpr" "$HOME/.nix-profile" &&
   rm -rf "$HOME/.local/.cache/nix" &&
   rm -rf "$HOME/.local/state/nix/profiles" &&
-  "$HOME/shared-configs/scripts/init/install_nix.bash" &&
+  "$HOME/shared-configs/scripts/install/install_nix.bash" &&
   echo "[Nix] Upgrade from v$INSTALLED_VERSION to v$RELEASE_VERSION successfully"
