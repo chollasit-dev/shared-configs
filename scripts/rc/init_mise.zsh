f2116911418fd1ab2e12fe6d97945301b0872197
chollasit-dev <118034127+chollasit-dev@users.noreply.github.com>
2026-01-30 00:26:29 +0700

    Minor refactor on initilize shell profile script

--- scripts/rc/init_mise.zsh
+++ scripts/rc/init_mise.zsh
@@ -2,10 +2,11 @@ command -v mise &>/dev/null &&
   eval "$(mise activate zsh)" &&
   eval "$(mise trust --all)"
 
-if command -v usage &>/dev/null; then
-  eval "$(mise completion zsh)" ||
-    print "${fg[blue]}[Mise] ${fg[red]}Failed to add shell completion${reset_color}"
-  print "${fg[blue]}[Mise] ${fg[green]}add shell completion successfully${reset_color}"
-else
-  print "${fg[blue]}[Mise] ${fg[red]}usage-cli not found, shell completion initialization will be skip${reset_color}"
-fi
+{
+  if ! command -v usage &>/dev/null; then
+    print "${fg[blue]}[Mise] ${fg[red]}usage-cli not found, skip adding shell completion${reset_color}"
+  else
+    eval "$(mise completion zsh)" &&
+      print "${fg[blue]}[Mise] ${fg[green]}add shell completion successfully${reset_color}"
+  fi
+} || print "${fg[blue]}[Mise] ${fg[red]}Failed to add shell completion${reset_color}"
